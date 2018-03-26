b7787d08ddfc4aec29e60a7a63671b2da0771485
Alexandri Anastasia Evgenia <alexandri.a.eugenia@gmail.com>
2018-03-26 19:14:23 +0300

    feat: Function that matches same icon cards work

--- js/app.js
+++ js/app.js
@@ -3,20 +3,21 @@ const cardIcons = ["fa fa-android","fa fa-android","fa fa-github-alt","fa fa-git
 
 const deck = document.querySelector('.deck');
 const cards = document.querySelectorAll('.card');
-const cardOpen = document.querySelectorAll('.open');
+
 const cardMatch = document.querySelectorAll('.match');
 const cardNoMatch = document.querySelectorAll('.no_match');
 
 //shufle cards(icons)
-const shuffledCards = shuffle(cards);
+const shuffledCards = shuffle(cardIcons);
 let openedCards = [];
 
 //create grid
-generateListItems();
+generateDashboard();
 
 //add one event listener to cards parent element <ul>
 deck.addEventListener( 'click', function (evt) {
-
+	openCard(evt);
+	check(evt);
 } );
 
 
@@ -56,8 +57,28 @@ function generateDashboard () {
 	deck.appendChild(fragment);
 }
 
+// Open card that is clicked and store its icon class to an array
 function openCard (x) {
     if (x.target.nodeName === 'LI') {  
        	x.target.classList.add("open");
+       	const clickedCardIconClass = x.target.childNodes[0].getAttribute("class");
+       	openedCards.push(clickedCardIconClass);
     }
+}
+
+// Check if cards match and act accordingly
+function check (x) {
+
+	if (openedCards.length == 2) {
+		if ( openedCards[0] === openedCards[1] ) {
+			let cardOpen = document.querySelectorAll('.open');
+			cardOpen[0].classList.add("match");
+			cardOpen[1].classList.add("match");
+			cardOpen[0].classList.remove("open");
+			cardOpen[1].classList.remove("open");
+			openedCards.splice(0,2);
+			}else{
+
+			}
+	}
 }
